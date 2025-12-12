e4ebc885d08b02b8b40cfa15988822088408d8b3
Morgan Chapleau-Stein <58056433+YuzuRanger@users.noreply.github.com>
2025-12-12 13:04:09 -0800

    implement double sided test reading

--- Half Sheet Dir/Half Sheet/grade_half_sheets_from_pdf.py
+++ Half Sheet Dir/Half Sheet/grade_half_sheets_from_pdf.py
@@ -12,6 +12,7 @@ from pdf2image import convert_from_path
 from PIL import Image, ImageDraw
 from datetime import datetime
 import math
+from pypdf import PdfReader
 
 # 1. Answer Key & Points 
 
@@ -96,7 +97,7 @@ def image_to_grid(img, student):
     # print(f"Grid layout with dark pixel detection saved as {output_path}.")
     return detection_results
 
-def rotate_image(image, student):
+def rotate_image(image, student, log_file):
     # Convert to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -112,7 +113,8 @@ def rotate_image(image, student):
         area = cv2.contourArea(cnt)
         if 2000 < area < 2220: # for the big rectangle markers
             x, y, w, h = cv2.boundingRect(cnt)
-            selected_contours.append((x, y, x + w, y + h))
+            if (h < 100): # need to ignore barcodes on back
+                selected_contours.append((x, y, x + w, y + h))
 
     # Convert image to PIL format
     image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
@@ -156,10 +158,10 @@ def rotate_image(image, student):
         # print(f"Image rotated by {angle:.2f} degrees using two contours.")
         return rotated
     else:
-        print("!! " + student + " Warning: Not enough contours found for rotation. Verify answers on " + student + " or re-scan.")
+        custom_print("!! " + student + " Warning: Wrong number of contours (" + str(len(selected_contours)) + ") found for rotation. Verify answers on " + student + " or re-scan.", log_file)
         return image
 
-def crop_to_answers(image, student):
+def crop_to_answers(image, student, log_file, IS_BACK_SIDE=False):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Threshold to isolate dark regions
@@ -174,7 +176,8 @@ def crop_to_answers(image, student):
         area = cv2.contourArea(cnt)
         if 2000 < area < 2220: # for the big rect markers
             x, y, w, h = cv2.boundingRect(cnt)
-            selected_contours.append((x, y, x + w, y + h))
+            if (h < 100): # need to ignore barcodes on back
+                selected_contours.append((x, y, x + w, y + h))
 
     # Convert image to PIL format
     image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
@@ -196,7 +199,10 @@ def crop_to_answers(image, student):
 
         # print("y corners:", y_corners)
         # print("x corners:", x_corners)
-        x_start, y_start, x_end, y_end  = x_corners[1] + 80, y_corners[1] + 35, x_corners[0] - 460, y_corners[0] - 115
+        if IS_BACK_SIDE:
+            x_start, y_start, x_end, y_end  = x_corners[1] + 80, y_corners[1] + 35, x_corners[0] - 460, y_corners[0] - 355
+        else:
+            x_start, y_start, x_end, y_end  = x_corners[1] + 80, y_corners[1] + 35, x_corners[0] - 460, y_corners[0] - 115
         image = image[y_start:y_end, x_start:x_end]
         # filename = "temp_image.png"
         # cv2.imwrite(filename, image)
@@ -210,10 +216,10 @@ def crop_to_answers(image, student):
         image = image[y_start:y_end, x_start:x_end]
         # filename = "temp_image_default.png"
         # cv2.imwrite(filename, image)
-        print("!! " + student + " Warning: Not enough contours found for cropping. Verify answers on " + student + ".")
+        custom_print("!! " + student + " Warning: Wrong number of contours (" + str(len(selected_contours)) + ") found for cropping. Verify answers on " + student + ".", log_file)
         return image
 
-def process_scanned_page(image, student): 
+def process_scanned_page(image, student, log_file): 
 
     """ 
     Processes a single image (one side of a scanned bubble form) and returns its score. 
@@ -228,29 +234,80 @@ def process_scanned_page(image, student):
     # for row, detections in enumerate(detection_results_grid.items()):
     # for i in range(len(ANSWER_KEY)):
     count_limit = len(ANSWER_KEY)
-    for row, detections in enumerate(detection_results_grid.items()):
-        if row >= count_limit:
-            break
-        #print(f"{row}: {detections}")
-        if True in detections[1]:
-            # print("true was found")
-            first_true_index = detections[1].index(True)
-            answers.append(CHOICE_INDEX[first_true_index])
-            # print("first_true_index", first_true_index)
-            correct_answer = ANSWER_KEY.get(row) 
-            # print("correct_answer", correct_answer)
-            if first_true_index == correct_answer:
-                # print("A CORRECT ANSWER!")
-                points = POINT_VALUES.get(row, DEFAULT_POINTS) 
-                page_score += points 
+    TWO_SIDED = 0
+    BACK_PAGE = 0
+    if count_limit > 50:
+        # this is a two sided situation
+        TWO_SIDED = 1
+        print(student)
+        page_number = str(student)
+        print(page_number)
+        page_number = student[-1]
+        page_number = int(page_number)
+        if page_number % 2 == 0:
+            BACK_PAGE = 1
+    if TWO_SIDED == 0:
+        for row, detections in enumerate(detection_results_grid.items()):
+            if row >= count_limit:
+                break
+            #print(f"{row}: {detections}")
+            if True in detections[1]:
+                # print("true was found")
+                first_true_index = detections[1].index(True)
+                answers.append(CHOICE_INDEX[first_true_index])
+                # print("first_true_index", first_true_index)
+                correct_answer = ANSWER_KEY.get(row) 
+                # print("correct_answer", correct_answer)
+                if first_true_index == correct_answer:
+                    # print("A CORRECT ANSWER!")
+                    points = POINT_VALUES.get(row, DEFAULT_POINTS) 
+                    page_score += points 
+            else:
+                #print("blank answer")
+                answers.append("BLANK")
+                custom_print("Blank answer detected for question:" + str(row) + "- Consider checking.", log_file)
+
+    else:
+        # two sided
+        if BACK_PAGE == 0:
+            count_limit = 50 # do the first 50 only
+            for row, detections in enumerate(detection_results_grid.items()):
+                if row >= count_limit:
+                    break
+                if True in detections[1]:
+                    first_true_index = detections[1].index(True)
+                    answers.append(CHOICE_INDEX[first_true_index])
+                    correct_answer = ANSWER_KEY.get(row) 
+                    if first_true_index == correct_answer:
+                        points = POINT_VALUES.get(row, DEFAULT_POINTS) 
+                        page_score += points 
+                else:
+                    answers.append("BLANK")
+                    custom_print("Blank answer detected for question:" + str(row) + "- Consider checking.", log_file)
         else:
-            #print("blank answer")
-            answers.append("BLANK")
+            for row, detections in enumerate(detection_results_grid.items()):
+                if row + 50 >= count_limit:
+                    break
+                if True in detections[1]:
+                    first_true_index = detections[1].index(True)
+                    answers.append(CHOICE_INDEX[first_true_index])
+                    correct_answer = ANSWER_KEY.get(row + 50) 
+                    if first_true_index == correct_answer:
+                        points = POINT_VALUES.get(row + 50, DEFAULT_POINTS) 
+                        page_score += points 
+                else:
+                    answers.append("BLANK")
+                    custom_print("Blank answer detected for question:" + str(row + 50) + "- Consider checking.", log_file)
     
     return [page_score, answers]
 
 def get_parent_dir(directory):
     return os.path.dirname(directory)
+
+def custom_print(message_to_print, log_file):
+    print(message_to_print)
+    with open(log_file, 'a') as of:
+        of.write(message_to_print + '\n')
 
 def main():
 
@@ -260,7 +317,8 @@ def main():
 
     answer_key_dir = script_dir + "\\Answer Key"
 
-    output_csv = script_dir + "\\output\\Half-Sheet_Results_" + str(datetime.now().strftime('%Y-%m-%d')) + ".csv"
+    output_csv = script_dir + "\\output\\Half-Sheet_Results_" + str(datetime.now().strftime('%Y-%m-%d-%H-%M')) + ".csv"
+    log_file = script_dir + "\\output\\Log_" + str(datetime.now().strftime('%Y-%m-%d-%H-%M')) + ".txt"
 
     # Delete any previous pngs saved of answers
     output_images_dir = script_dir + "\\Output"
@@ -284,14 +342,24 @@ def main():
                     if row_dict['Points']:
                         POINT_VALUES[index] = int(row_dict['Points'])
 
-        else:
-            print(f"!! No answer key csv found in '{answer_key_dir}'")
-            input("Press Enter to exit...")
-            # sys.exit(1)
+        # else:
+        #     print(f"!! No answer key csv found in '{answer_key_dir}'")
+        #     input("Press Enter to exit...")
+        #     # sys.exit(1)
 
-
+    DOUBLE_SIDED_FLAG = 0
     # print(ANSWER_KEY)
     # print(POINT_VALUES)
+    user_input = input("Is your test double-sided/greater than 50 questions? Enter the letter y for yes, or nothing for no, and press enter: ")
+    if user_input:
+        if user_input[0].lower() == 'y':
+            custom_print("User indicated double-sided/greater than 50 questions.", log_file)
+            DOUBLE_SIDED_FLAG = 1
+        else:
+            custom_print("Unexpected selection. Defaulting to single-sided/50 questions or less.", log_file)
+    else:
+        custom_print("User indicated single-sided/50 questions or less.", log_file)
+    
 
     with open(output_csv, 'w', newline='') as f: 
 
@@ -312,32 +380,88 @@ def main():
                 pdf_path = os.path.join(input_dir, filename) 
                 local_poppler_path = script_dir + "\\poppler-25.07.0\\Library\\bin"
 
-                try: 
-                    pages_as_images = convert_from_path(pdf_path, dpi=300, poppler_path=local_poppler_path) 
+                # try:
+                reader = PdfReader(pdf_path)
+                num_pages = len(reader.pages)
+                print(f"The PDF has {num_pages} pages.")
 
+                pages_as_images = list()
+                page_floor = num_pages // 50
+                page_modulo = num_pages % 50
+                start_page = 1
+                if page_floor > 0:
+                    for i in range(page_floor):
+                        end_page = start_page + 49
+                        print(f"Processing pages_as_images for {start_page} through {end_page} of {num_pages}...")
+                        pages_as_images.extend(convert_from_path(pdf_path, dpi=300, poppler_path=local_poppler_path,
+                                                                    first_page=start_page, last_page=end_page))
+                        start_page += 50
+                if page_modulo > 0:
+                    end_page = start_page + page_modulo - 1
+                    print(f"Processing pages_as_images for {start_page} through {end_page} of {num_pages}...")
+                    pages_as_images.extend(convert_from_path(pdf_path, dpi=300, poppler_path=local_poppler_path,
+                                        first_page=start_page, last_page=end_page))
+
+                print("PDF read successfully. Preparing to grade.")
+                if DOUBLE_SIDED_FLAG == 0:
                     for i, page_image_pil in enumerate(pages_as_images): 
                         student = f"Page_{i+1}"
-                        # Convert PIL image to OpenCV format (numpy array) 
+                        custom_print(student, log_file)
+
                         page_image_cv = cv2.cvtColor(np.array(page_image_pil), cv2.COLOR_RGB2BGR) 
-                        # filename = "page_image_cv.png"
-                        # cv2.imwrite(filename, page_image_cv)
+                        page_image_cv = rotate_image(page_image_cv, student, log_file)
+                        page_image_cv = crop_to_answers(page_image_cv, student, log_file)
 
-                        page_image_cv = rotate_image(page_image_cv, student)
-                        page_image_cv = crop_to_answers(page_image_cv, student)
-
-                        exam_results = process_scanned_page(page_image_cv, student) 
+                        exam_results = process_scanned_page(page_image_cv, student, log_file) 
                         
                         student_row = [student, exam_results[0]]
                         student_row.extend(exam_results[1])
+                        writer.writerow(student_row)
+                else:
+                    temp_exam_results = []
+                    for i, page_image_pil in enumerate(pages_as_images): 
+                        if (i + 1) % 2 == 0:
+                            # the second/back page
+                            student = f"Page_{i+1}"
+                            custom_print(student, log_file)
+                            page_image_cv = cv2.cvtColor(np.array(page_image_pil), cv2.COLOR_RGB2BGR) 
+                            page_image_cv = rotate_image(page_image_cv, student, log_file)
+                            page_image_cv = crop_to_answers(page_image_cv, student, log_file, True)
+                            
+                            back_page_results = process_scanned_page(page_image_cv, student, log_file)
+                            # print(back_page_results)
+                            # print(temp_exam_results)
 
-                        # print(f"  - Page {i+1} of {filename}: {exam_results[0]} points") 
-                        writer.writerow(student_row) 
+                            score1 = temp_exam_results[0]
+                            score2 = back_page_results[0]
 
-                except Exception as e: 
-                    print(f"!! ERROR processing {filename}: {e}") 
-                    writer.writerow(["ERROR"]) 
+                            # print("score 1:", score1)
+                            # print("score 2:", score2)
 
-    print(f"Batch processing complete. Scores saved to '{output_csv}'.") 
+                            score = int(score1) + int(score2)
+                            answers = temp_exam_results[1]
+                            answers.extend(back_page_results[1])
+
+                            student_row = [student, score]
+                            student_row.extend(answers)
+                            writer.writerow(student_row)
+                        else:
+                            # a new student
+                            student = f"Page_{i+1}"
+                            temp_exam_results.clear()
+                            custom_print(student, log_file)
+                            page_image_cv = cv2.cvtColor(np.array(page_image_pil), cv2.COLOR_RGB2BGR) 
+                            page_image_cv = rotate_image(page_image_cv, student, log_file)
+                            page_image_cv = crop_to_answers(page_image_cv, student, log_file)
+
+                            temp_exam_results = process_scanned_page(page_image_cv, student, log_file)
+
+
+                # except Exception as e: 
+                #     print(f"!! ERROR processing {filename}: {e}") 
+                #     writer.writerow(["ERROR"]) 
+
+    print(f"Batch processing complete. Scores saved to '{output_csv}'. Scores saved to '{output_csv}'. Console alerts saved to '{log_file}'.") 
     input("Press Enter to exit...")
     # sys.exit(0)
 
